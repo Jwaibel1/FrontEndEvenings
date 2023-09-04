6f4e7e5f63495b7b430bd5f5a8848e9a86bc204c
Jwaibel1 <joshuawaibel8@gmail.com>
2023-09-04 11:35:03 -0500

    Class Session 4

---
+++ Class-Session-4/Exercise_3/main.js
@@ -0,0 +1,9 @@
+const person = {
+  name: "Josh",
+  age: 26,
+  gender: "male",
+};
+
+console.log(person.name);
+console.log(person.age);
+console.log(person.gender);
---
+++ Class-Session-4/Exercise_4/main.js
@@ -0,0 +1,7 @@
+const car = {
+  make: "Chevrolet",
+  model: "Silverado",
+  year: "2002",
+};
+
+console.log(car.make, car.model, car.year);
