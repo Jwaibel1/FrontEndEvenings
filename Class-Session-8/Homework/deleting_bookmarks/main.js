5c3fb26e2f6c31c3207586eafadd321f245fdad5
Jwaibel1 <joshuawaibel8@gmail.com>
2023-09-20 20:41:08 -0500

    Class Session 8 Homework/Exercises

---
+++ Class-Session-8/Homework/deleting_bookmarks/main.js
@@ -0,0 +1,99 @@
+const body = document.body;
+const input = document.querySelector("input[type=text]");
+const overlay = document.querySelector(".overlay");
+
+function showFloater() {
+  body.classList.add("show-floater");
+}
+
+function closeFloater() {
+  if (body.classList.contains("show-floater")) {
+    body.classList.remove("show-floater");
+  }
+}
+
+input.addEventListener("focusin", showFloater);
+input.addEventListener("focusout", closeFloater);
+overlay.addEventListener("click", closeFloater);
+
+// ==================================================
+
+const bookmarksList = document.querySelector(".bookmarks-list");
+const bookmarkForm = document.querySelector(".bookmark-form");
+const bookmarkInput = document.querySelector("input[type=text]");
+const bookmarks = JSON.parse(localStorage.getItem("bookmarks")) || [];
+
+fillBookmarksList(bookmarks);
+
+function createBookmark(event) {
+  event.preventDefault();
+
+  // add a new bookmark to the bookmarks
+  const title = bookmarkInput.value;
+  const bookmark = {
+    title: title,
+  };
+  bookmarks.push(bookmark);
+  fillBookmarksList(bookmarks);
+  storeBookmarks(bookmarks);
+  bookmarkForm.reset();
+
+  console.table(bookmarks);
+
+  // save that bookmarks list to localStorage
+
+  /* const title = bookmarkInput.value;
+  const bookmark = document.createElement("a");
+  bookmark.className = "bookmark";
+  bookmark.innerText = title;
+  bookmark.href = "#";
+  bookmark.target = "_blank";
+  bookmarksList.appendChild(bookmark); */
+}
+
+function fillBookmarksList(bookmarks = []) {
+  const bookmarksHtml = bookmarks
+    .map((bookmark, i) => {
+      return `<a href="#" class="bookmark" data-id="${i}">
+      <div class="img"></div>
+      <div class="title">${bookmark.title}</div>
+      <span class="glyphicon glyphicon-remove"></span>
+      </a>`;
+    })
+    .join("");
+  bookmarksList.innerHTML = bookmarksHtml;
+
+  /* let bookmarksHtml = "";
+  for (i = 0; i < bookmarks.length; i++) {
+    bookmarksHtml += `
+      <a href="#" class="bookmark">
+        ${bookmarks[i].title}
+      </a>
+    `;
+  } */
+
+  /* console.log(bookmarksHtml); */
+}
+
+function removeBookmark(event) {
+  if (!event.target.matches(".glyphicon-remove")) return;
+
+  // find the index
+  // remove from the bookmarks using splice
+  // fill the list
+  // store back to locaStorage
+
+  const index = event.target.parentNode.dataset.id;
+  bookmarks.splice(index, 1);
+  fillBookmarksList(bookmarks);
+  storeBookmarks(bookmarks);
+
+  console.dir(index);
+}
+
+function storeBookmarks(bookmarks = []) {
+  localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+}
+
+bookmarkForm.addEventListener("submit", createBookmark);
+bookmarksList.addEventListener("click", removeBookmark);
